0bd035ef9eb3a1c091c9866e632a66d1ff44496c
hyeju1123 <hjjun1123@gmail.com>
2022-12-21 23:43:49 +0900

    card origami - unnatural motion

--- CardOrigamiPage.js
+++ CardOrigamiPage.js
@@ -1,27 +1,42 @@
 import React, { useRef, useEffect } from "react";
-import {
-    View,
-    Animated,
-    StyleSheet,
-    Dimensions,
-    Text,
-    Image,
-} from "react-native";
+import { View, Animated, StyleSheet, Dimensions, Text } from "react-native";
 
 export const CardOrigamiPage = () => {
     const cardAnimated = useRef(new Animated.Value(0)).current;
+    const colorAnimated = useRef(new Animated.Value(0)).current;
+    const revealColorAnimated = useRef(new Animated.Value(0)).current;
+
     const cardRotate = cardAnimated.interpolate({
         inputRange: [0, 1],
         outputRange: ["0deg", "-180deg"],
     });
+    const colorRotate = colorAnimated.interpolate({
+        inputRange: [0, 150],
+        outputRange: [1, 0],
+        extrapolate: "clamp",
+    });
+    const revealColorRotate = revealColorAnimated.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1],
+        extrapolate: "clamp",
+    });
 
     const cardFlipAni = () => {
-        Animated.spring(cardAnimated, {
-            toValue: 1,
-            friction: 4,
-            tension: 4,
-            useNativeDriver: true,
-        }).start();
+        Animated.loop(
+            Animated.parallel([
+                Animated.spring(cardAnimated, {
+                    toValue: 1,
+                    friction: 4,
+                    tension: 0,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(revealColorAnimated, {
+                    toValue: 150,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+            ])
+        ).start();
     };
 
     useEffect(() => {
@@ -35,15 +50,26 @@ export const CardOrigamiPage = () => {
                     <View style={styles.pointInCard} />
                 </View>
                 <Animated.View
-                    style={{
-                        transform: [
-                            { rotateX: cardRotate },
-                            { rotateY: cardRotate },
-                        ],
-                    }}
-                >
-                    <View style={styles.foldedCardPart}></View>
-                </Animated.View>
+                    style={[
+                        styles.foldedCardPart,
+                        {
+                            transform: [
+                                { rotateX: cardRotate },
+                                { rotateY: cardRotate },
+                            ],
+                            borderColor: "#d42361",
+                        },
+                    ]}
+                ></Animated.View>
+                <Animated.View
+                    style={[
+                        styles.foldedCardPart2,
+                        {
+                            opacity: revealColorRotate,
+                            borderColor: "#d42361",
+                        },
+                    ]}
+                ></Animated.View>
             </View>
             <Text style={styles.text}>결제 정보 확인 중</Text>
         </View>
@@ -85,9 +111,13 @@ const styles = StyleSheet.create({
     foldedCardPart: {
         width: width * 0.1,
         height: width * 0.1,
-        borderLeftColor: "#000",
-        borderTopColor: "#000",
         borderLeftWidth: 2,
         borderTopWidth: 2,
+    },
+    foldedCardPart2: {
+        width: width * 0.1,
+        height: width * 0.1,
+        borderTopWidth: 2,
+        right: width * 0.1,
     },
 });
